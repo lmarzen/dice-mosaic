97ac1979bec76254a5effe04b63a992481497b08
Luke Marzen <marzenlj@gmail.com>
2022-01-28 19:13:33 -0600

    Added stb_image_resize library, .gitignore .vscode

--- main.c
+++ main.c
@@ -6,6 +6,22 @@
 #include "stb_image/stb_image.h"
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image/stb_image_write.h"
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image/stb_image_resize.h"
+
+// debug
+#include <sys/time.h>
+
+/*
+  struct timeval start, end;
+  double time_taken = 0;
+  gettimeofday(&start, NULL); // start timer
+
+  gettimeofday(&end, NULL); // stop timer
+  time_taken = end.tv_sec + end.tv_usec / 1e6 -
+               start.tv_sec - start.tv_usec / 1e6; // in seconds
+  printf("program took an average of %f seconds to execute\n", time_taken / 100);
+*/
 
 int main(int argc, char *argv[])
 {
@@ -20,9 +36,28 @@ int main(int argc, char *argv[])
   }
   printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
 
-  stbi_write_jpg("test.jpg", width, height, channels, img, 80);
+  // Convert the input image to grayscale
+  size_t img_size = width * height * channels;
+  int gray_channels = channels == 4 ? 2 : 1;
+  size_t gray_img_size = width * height * gray_channels;
 
-  stbi_image_free(img);    
+  unsigned char *gray_img = malloc(gray_img_size);
+  if(gray_img == NULL) {
+      printf("Unable to allocate memory for the gray image.\n");
+      exit(1);
+  }
+
+  for(unsigned char *p = img, *pg = gray_img; p != img + img_size; p += channels, pg += gray_channels) {
+    *pg = (uint8_t)((*p + *(p + 1) + *(p + 2))/3.0);
+    if(channels == 4) {
+      *(pg + 1) = *(p + 3);
+    }
+  }
+
+  stbi_write_jpg("./photos/output.jpg", width, height, gray_channels, gray_img, 80);
+
+  stbi_image_free(img);
+  stbi_image_free(gray_img);    
 
 return 0;
 }
