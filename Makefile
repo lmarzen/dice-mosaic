e7328988845b39142e82b8fd686f4106e597b84a
Luke Marzen <marzenlj@gmail.com>
2022-01-28 18:53:00 -0600

    Added stb_image libraries

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 dice-mosaic: main.c
-	gcc main.c -o dice-mosaic -Wall -Werror -O3 -lpng
+	gcc main.c -o dice-mosaic -Wall -Werror -O3 -lm
 
 clean:
 	rm -f *- dice-mosaic core
--- main.c
+++ main.c
@@ -1,39 +1,29 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <png.h>
 
-
-png_infop info_ptr;
-png_bytepp row_pointers; 
-
-void read_png(char *file_name)
-{
-  FILE *fp = fopen(file_name, "rb");
-  png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-  info_ptr = png_create_info_struct(png_ptr);  
-  png_init_io(png_ptr, fp);
-  png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
-  row_pointers = png_get_rows(png_ptr, info_ptr);
-  png_destroy_read_struct(&png_ptr, NULL, NULL); 
-  fclose(fp);
-}
-
-void write_png(char *file_name)
-{
-  FILE *fp = fopen(file_name, "wb");
-  png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
-  png_init_io(png_ptr, fp);
-  png_set_rows(png_ptr, info_ptr, row_pointers);
-  png_write_png(png_ptr, info_ptr, PNG_TRANSFORM_IDENTITY, NULL);
-  png_destroy_write_struct(&png_ptr, &info_ptr);
-  fclose(fp);
-}
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image/stb_image_write.h"
 
 int main(int argc, char *argv[])
 {
-  read_png(argv[1]);
-  write_png(argv[2]);
-  return 0;
+  //read_png(argv[1]);
+  //write_png(argv[2]);
+
+  int width, height, channels;
+  unsigned char *img = stbi_load(argv[1], &width, &height, &channels, 0);
+  if(img == NULL) {
+    printf("Error in loading the image\n");
+    exit(1);
+  }
+  printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
+
+  stbi_write_jpg("test.jpg", width, height, channels, img, 80);
+
+  stbi_image_free(img);    
+
+return 0;
 }
 
